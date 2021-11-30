731f931866fd03009c1de2fa2e8949ecdac3bd2b
Mathis <mathis.duban@etu.umontpellier.fr>
2021-11-30 22:27:52 +0100

    Initial commit

---
+++ src/Date.java
@@ -0,0 +1,45 @@
+public class Date {
+
+    private final String[] month = {"janvier", "fevier", "mars", "avril", "mai", "juin", "juillet", "aout", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"};
+    private final int day;
+    private final int year;
+    private final int months;
+
+
+    public Date(int jour, int mois, int annee) {
+        this.day = jour;
+        this.months = mois;
+        this.year = annee;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public String getMonths() {
+        return month[months - 1];
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void printdate() {
+        System.out.println(this.day + " " + this.getMonths() + " " + this.getYear());
+    }
+
+    public boolean isbisex() {
+        boolean bis = false;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            bis = !bis;
+        }
+        // year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+        return bis;
+    }
+
+    public void adddate() {
+//        this.day=this.day+1;
+    }
+
+
+}
---
+++ src/DateMain.java
@@ -0,0 +1,13 @@
+public class DateMain {
+
+    public static void main(String[] args) {
+        Date d1 = new Date(1, 3, 1900);
+        Date d2 = new Date(1, 3, 2000);
+
+        Date d3 = new Date(1, 3, 2016);
+        System.out.println(d1.isbisex());
+        System.out.println(d2.isbisex());
+        System.out.println(d3.isbisex());
+     d1.adddate();
+    }
+}
