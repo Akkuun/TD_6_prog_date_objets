54649c3721df49fcd3f2e6679c22ff6ffcaa5769
Mathis <mathis.duban@etu.umontpellier.fr>
2021-12-01 13:32:41 +0100

    suite

--- src/Date.java
+++ src/Date.java
@@ -1,6 +1,6 @@
 public class Date {
 
-    private static final String[] month = {"janvier", "fevier", "mars", "avril", "mai", "juin", "juillet", "aout", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"};
+    private static final String[] month = {"janvier", "fevier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"};
     //static relatif à la classe global
     //final = contante on peut pas le redefinir
     private int[] maxjours = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -43,15 +43,34 @@ public class Date {
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0; //si c'est le cas bah ça renvoie vrai sinon non
     }
 
-    public void addjour() {
+    public Date addjour() {
 
-        day=day+1;
+        day = day + 1;
 
 
-        if (day > maxjours[months]) {
-        months=months+1;
+        if (day > maxjours[months - 1]) {
+            months ++;
+            day = 1;
+            if (months > 12) {
+                year ++;
+                months = 1;
+            }
         }
+        return this;
     }
 
+    public Date lendemain() {
+        Date d1 = this;
+
+        return new Date(d1.day, d1.months, d1.year).addjour();
+    }
+    public boolean testdate(Date d1,Date d2){
+        //if d1.day = d2.day ==true return true
+        boolean issame=false;
+        if (d1.day!=d2.day&&d1.months!= d2.months&&d1.year!=d2.year){
+            issame=!issame;
+        }
+        return issame;
+    }
 
 }
--- src/DateMain.java
+++ src/DateMain.java
@@ -1,14 +1,18 @@
 public class DateMain {
 
     public static void main(String[] args) {
-        Date d1 = new Date(1, 3, 1900);
-        Date d2 = new Date(1, 3, 2000);
+        Date d4;
+        Date d1 = new Date(29, 2, 2024);
+        Date d2 = new Date(31, 12, 2000);
 
         Date d3 = new Date(1, 3, 2016);
         System.out.println(d1.isbisex());
         System.out.println(d2.isbisex());
         System.out.println(d3.isbisex());
-        d1.addjour();
-        d1.printdate();
+        d2.printdate();
+        d2.addjour();
+        d2.printdate();
+        d1.lendemain();
+        d4=d4.lendemain();
     }
 }
