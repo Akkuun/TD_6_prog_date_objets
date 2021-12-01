ae5f10dc920533733415fbda3050d96d72ca6f14
Mathis <mathis.duban@etu.umontpellier.fr>
2021-12-01 10:10:50 +0100

    suite

--- src/Date.java
+++ src/Date.java
@@ -1,15 +1,21 @@
 public class Date {
 
-    private final String[] month = {"janvier", "fevier", "mars", "avril", "mai", "juin", "juillet", "aout", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"};
-    private  int day;
-    private  int year;
-    private  int months;
+    private static final String[] month = {"janvier", "fevier", "mars", "avril", "mai", "juin", "juillet", "aout", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"};
+    //static relatif à la classe global
+    //final = contante on peut pas le redefinir
+    private int[] maxjours = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private int day;
+    private int year;
+    private int months;
 
 
     public Date(int jour, int mois, int annee) {
-        this.day = jour;
-        this.months = mois;
-        this.year = annee;
+
+        maxjours[1] = isbisex() ? 29 : 28; //is isbisex=true maxjour[1] =29 else maxjour[1] =28
+
+        day = jour;
+        months = mois;
+        year = annee;
     }
 
     public int getDay() {
@@ -17,28 +23,34 @@ public class Date {
     }
 
     public String getMonths() {
+
         return month[months - 1];
     }
 
+
     public int getYear() {
+
         return year;
     }
 
     public void printdate() {
-        System.out.println(this.day + " " + this.getMonths() + " " + this.getYear());
+
+        System.out.println(this.getDay() + " " + this.getMonths() + " " + this.getYear());
     }
 
     public boolean isbisex() {
-        boolean bis = false;
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            bis = !bis;
-        }
-        // year % 4 == 0 && year % 100 != 0 || year % 400 == 0
-        return bis;
+
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0; //si c'est le cas bah ça renvoie vrai sinon non
     }
 
-    public void adddate() {
-        this.day=this.day+1;
+    public void addjour() {
+
+        day=day+1;
+
+
+        if (day > maxjours[months]) {
+        months=months+1;
+        }
     }
 
 
